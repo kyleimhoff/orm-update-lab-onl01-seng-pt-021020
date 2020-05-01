6fda66196fabd3727834cb95d9a96cfafefd61d5
Kyle Imhoff <imhoff.kyle@gmail.com>
2020-05-01 19:20:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -27,6 +27,13 @@ class Student
     
   end
   def self.save
-    
+    if self.id 
+      self.update
+    else
+      sql = <<-SQL
+      INSERT INTO students (name, grade)
+      VALUES (?, ?)
+      SQL
+      DB[:conn].execute(sql, self.name, self.grade)
 
 end
