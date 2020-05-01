7adb792aa3c2c27fe213479bf0a1d8dd1c247ab0
Kyle Imhoff <imhoff.kyle@gmail.com>
2020-05-01 19:31:19 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -44,7 +44,21 @@ class Student
     student
   end
   def self.new_from_db(row)
-    
+    student = self.new 
+    student.id = row[0]
+    student.name = row[1]
+    student.grade = row[2]
+    student
   end 
+  def self.find_by_name(name)
+    sql = <<-SQL 
+    SELECT * FROM students WHERE name = ? LIMIT 1
+    SQL
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first 
+  end
+  def update
+      
 
 end
